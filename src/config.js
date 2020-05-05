6a6631782eeeed1d38891e1bf10ceb6dcd946243
Bibi <bibi.samreny@protonmail.com>
2020-05-04 23:49:16 -0700

    x

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
 exports.PORT = process.env.PORT || 8080;
-exports.API_BASE_URL = "http://localhost:8080" ;
-// process.env.REACT_APP_API_BASE_URL || 
+exports.API_BASE_URL = "http://localhost:8080" || process.env.REACT_APP_API_BASE_URL
